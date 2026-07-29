55b09381078a0ca67c085bbcda071028f6d48fed
Piyush Suthar <piyushsuthar100@gmail.com>
2026-07-30 02:33:07 +0530

    testing different hyperparameters

--- transformer/v2.py
+++ transformer/v2.py
@@ -3,27 +3,29 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # Hyperparameters
-# batch_size = 32
-# block_size = 8
-# max_iters = 5000
-# eval_interval = 300
-# learning_rate = 1e-3
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# eval_iters = 200
-# n_embd = 32
-# n_layer = 3
-# n_head = 4
 batch_size = 64
-block_size = 256
+block_size = 128
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_layer = 6
-n_head = 6
+n_embd = 64
+n_layer = 3
+n_head = 4
 dropout = 0.2
+# below are too big of hyperparamters to train on my pc
+# batch_size = 64
+# block_size = 256
+# max_iters = 5000
+# eval_interval = 500
+# learning_rate = 3e-4
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# eval_iters = 200
+# n_embd = 384
+# n_layer = 6
+# n_head = 6
+# dropout = 0.2
 # --------------------
 
 torch.manual_seed(1337)
